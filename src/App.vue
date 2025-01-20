5ff324696c5b698de5ca368bf1287da5dff4b6ee
tdy <2015214273@qq.com>
2025-01-20 20:07:47 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts">
+import { onLaunch, onShow, onHide } from "@dcloudio/uni-app";
+
+</script>
+<style></style>
---
+++ src/pages/index/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <view>11</view>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style>
+
+</style>
