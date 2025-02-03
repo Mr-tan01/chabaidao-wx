9cb28e67872ed9bb1bbc8e4f8c9d9efe48ec8897
tdy <2015214273@qq.com>
2025-02-03 16:46:17 +0800

    收货地址功能

--- src/types/userinfo.d.ts
+++ src/types/userinfo.d.ts
@@ -1,7 +1,16 @@
 // 用户登录信息类型
-export interface UserInfo {
+export type UserInfo = {
   avatar: string
   nickname: string
   user_Token: string
+  _id: string
+}
+// 收获地址类型
+export type Address = {
+  address: string
+  defaultAddress: boolean
+  detailedAddress: string
+  mobile: string
+  name: string
   _id: string
 }
